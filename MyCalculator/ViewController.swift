b974d6672ec4ddb93cc4793bd25ae6ee0378f427
Zoe <zoe@lionce.com>
2015-06-10 00:08:37 -0400

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Zoe on 6/10/15.
+//  Copyright (c) 2015 Z.S.Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
